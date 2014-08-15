88e5d4244273ce0e600b7b1ee1adb64f17a499c5
janardhanmadem <janardhan.madem@gmail.com>
2014-08-15 16:53:39 +0530

    i added sys

--- Try/src/LoginDao.java
+++ Try/src/LoginDao.java
@@ -15,7 +15,8 @@ Class.forName("com.mysql.jdbc.Driver").newInstance();
 Connection con=DriverManager.getConnection("jdbc:mysql://intranet.myplex.in:3306/bugs?"+"user=bugs&password=Apa1yaBug5");	
 PreparedStatement ps=con.prepareStatement( "select username from resources where username=? and password=MD5(?)");  
 ps.setString(1,name);  
-ps.setString(2,pass);  
+ps.setString(2,pass); 
+System.out.println("");
       
 ResultSet rs=ps.executeQuery();  
 status=rs.next();
